53bd3171239f9e8a43ae90a2856bb38841dbba22
allansm <37338692+allansm@users.noreply.github.com>
2018-04-14 13:37:42 -0300

    update files

--- gui/Cadastro.py
+++ gui/Cadastro.py
@@ -9,32 +9,35 @@ class Cadastro:
     def execute(self):
         cadastro = tkinter.Tk()
         cadastro.title("DrinkWater/Cadastro")
-        cadastro.geometry("400x400")
+        cadastro.geometry("400x340")
         cadastro.resizable(False, False)
         x = 90
         y = 30
         label =ttk.Label(cadastro,text = "login" ,font=20)
         label.place(x=x,y=y)
         input1 = ttk.Entry(cadastro,width=35)
-        input1.place(x=x,y=y+50)
+        input1.place(x=x,y=y+30)
         label = ttk.Label(cadastro,text="senha",font=20)
-        label.place(x=x, y=y+100)
+        label.place(x=x, y=y+80)
         input2 = ttk.Entry(cadastro,width=35 ,show="*")
-        input2.place(x=x, y=y+150)
+        input2.place(x=x, y=y+110)
         label = ttk.Label(cadastro,text="Confirmar Senha", font=20)
-        label.place(x=x, y=y + 200)
+        label.place(x=x, y=y + 160)
         input3 = ttk.Entry(cadastro,width=35, show="*")
-        input3.place(x=x, y=y + 250)
+        input3.place(x=x, y=y + 190)
         def cadastrar():
             u = User()
             u.setLogin(input1.get())
             u.setPassword(input2.get())
-            if (u.getPassword() == input3.get()):
-                UserDao().insert(u)
-                messagebox.showinfo("Aviso","usuario cadastrado")
+            if(u.getLogin() != "" and u.getPassword() != ""):
+                if (u.getPassword() == input3.get()):
+                    UserDao().insert(u)
+                    messagebox.showinfo("Aviso","usuario cadastrado")
+                else:
+                    messagebox.showinfo("Erro","erro ao cadastrar")
             else:
-                messagebox.showinfo("Aviso","erro ao cadastrar")
+                messagebox.showinfo("Erro","usuario e senha invalidos")
         button = ttk.Button(cadastro,text="Cadastrar" ,command=cadastrar)
-        button.place(x=x,y=y+300)
+        button.place(x=x,y=y+240)
         cadastro.mainloop()
 
--- gui/Main.py
+++ gui/Main.py
@@ -18,29 +18,29 @@ class Main(tkinter.Tk):
         return user
     def execute(self):
         self.title("DrinkWater/Login")
-        self.geometry("400x300")
+        self.geometry("400x260")
         self.resizable(False, False)
         x = 90
         y = 30
-        label =ttk.Label(text = "login" ,font=20)
+        label =ttk.Label(text = "Login" ,font=20)
         label.place(x=x,y=y)
         login = tkinter.StringVar()
         input = ttk.Entry(width=35,textvariable=login)
-        input.place(x=x,y=y+50)
-        label = ttk.Label(text="senha",font=20)
-        label.place(x=x, y=y+100)
+        input.place(x=x,y=y+30)
+        label = ttk.Label(text="Senha",font=20)
+        label.place(x=x, y=y+80)
         senha = tkinter.StringVar()
         input = ttk.Entry(width=35 ,textvariable=senha,show="*")
-        input.place(x=x, y=y+150)
+        input.place(x=x, y=y+110)
         def autenticar():
             user = self.auth(login.get(),senha.get())
             Painel().execute(user)
         button = ttk.Button(text="Autenticar" ,command=autenticar)
-        button.place(x=x,y=y+200)
+        button.place(x=x,y=y+160)
         def cadastro():
            Cadastro().execute()
         button = ttk.Button(text="Cadastrar" ,command=cadastro)
-        button.place(x=x+140, y=y + 200)
+        button.place(x=x+140, y=y + 160)
         self.mainloop()
 main = Main()
 main.execute()
